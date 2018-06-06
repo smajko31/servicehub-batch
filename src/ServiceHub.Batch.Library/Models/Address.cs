8a9f845421922ec97aedae0ef40a497e0f5fbcbe
Sebastian Majko <smajko31@gmail.com>
2018-06-06 17:43:42 -0400

    add Address library model

---
+++ src/ServiceHub.Batch.Library/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceHub.Batch.Library.Models
+{
+    public class Address
+    {
+        public Guid AddressId { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
