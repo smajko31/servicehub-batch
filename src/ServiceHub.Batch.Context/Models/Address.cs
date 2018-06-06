5d203a4b562dd67d6d727ceb999737baead4bc10
Sebastian Majko <smajko31@gmail.com>
2018-06-06 19:46:30 -0400

    add Address/Batch data models

---
+++ src/ServiceHub.Batch.Context/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ServiceHub.Batch.Context.Models
+{
+    public class Address
+    {
+        public Guid AddressId { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ src/ServiceHub.Batch.Context/Models/Batch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceHub.Batch.Context.Models
+{
+    public class Batch
+    {
+        // Unique batch id
+        public Guid BatchId { get; set; }
+        // Start date of training
+        public DateTime StartDate { get; set; }
+        // Expected training end date
+        public DateTime EndDate { get; set; }
+        // Specific name of batch
+        public string BatchName { get; set; }
+        // Total number of people in batch
+        public int BatchOccupancy { get; set; }
+        // Batch technology stack 
+        public string BatchSkill { get; set; }
+        // Address where training takes place
+        public Address Address { get; set; }
+        // List of associate ids in batch
+        public List<Guid> UserIds { get; set; }
+    }
+}
