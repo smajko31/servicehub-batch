205c7dd07d04925d69ec37fd535920f3a12b304a
Sebastian Majko <smajko31@gmail.com>
2018-06-07 18:51:10 -0400

    add interface for CRUD operations

---
+++ src/ServiceHub.Batch.Context/Utilities/IUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceHub.Batch.Context.Utilities
+{
+    /// <summary>
+    /// Basic CRUD interface for Batch models
+    /// All storage implementations must inherit IUtility
+    /// </summary>
+    public interface IUtility
+    {
+        Models.Batch GetBatchById(Guid id);
+        List<Models.Batch> GetAllBatches();
+        void AddBatch(Models.Batch batch);
+        void UpdateBatch(Models.Batch batch);
+        void DeleteBatch(Guid id);
+    }
+}
