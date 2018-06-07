e2e1a5cfcb2fbf988df79329616a5f2d08502fc6
Sebastian Majko <smajko31@gmail.com>
2018-06-07 11:48:32 -0400

    add model validations [#157252772]

--- src/ServiceHub.Batch.Library/Models/Address.cs
+++ src/ServiceHub.Batch.Library/Models/Address.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServiceHub.Batch.Library.Models
 {
     public class Address
     {
+        [Required]
         public Guid AddressId { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Address1 { get; set; }
         public string Address2 { get; set; }
+        [Required]
+        [StringLength(25)]
         public string City { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
         public string State { get; set; }
+        [Required]
+        [StringLength(5, MinimumLength = 5)]
         public string PostalCode { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
         public string Country { get; set; }
     }
 }
--- src/ServiceHub.Batch.Library/Models/Batch.cs
+++ src/ServiceHub.Batch.Library/Models/Batch.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServiceHub.Batch.Library.Models
 {
     public class Batch
     {
         // Unique batch id
+        [Required]
         public Guid BatchId { get; set; }
         // Start date of training
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
         // Expected training end date
+        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
         // Specific name of batch
+        [StringLength(50)]
         public string BatchName { get; set; }
         // Total number of people in batch
+        [Range(0, 100)]
         public int BatchOccupancy { get; set; }
         // Batch technology stack 
+        [StringLength(50)]
         public string BatchSkill { get; set; }
         // Address where training takes place
         public Address Address { get; set; }
