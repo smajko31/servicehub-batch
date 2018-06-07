78892cab39c0e236dfca8a91fa44346914d34f89
Sebastian Majko <smajko31@gmail.com>
2018-06-07 18:34:08 -0400

    add unannotated mongodb context models

---
+++ src/ServiceHub.Batch.Context/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceHub.Batch.Context.Models
+{
+    /// <summary>
+    /// Address model for repository
+    /// Contains all information about location.
+    /// </summary>
+    /// <remarks>
+    /// Used in Batch model to specify location of training.
+    /// </remarks>
+    public class Address
+    {
+        public Guid AddressId { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
