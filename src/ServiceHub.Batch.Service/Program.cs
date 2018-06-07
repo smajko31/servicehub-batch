fd54acc9c83bc2650b6498c784a9c76ede596130
fred belotte <fred@belotte.xyz>
2018-06-06 23:42:19 -0400

    remove unused using statement. [#158136355]

--- src/ServiceHub.Batch.Service/Program.cs
+++ src/ServiceHub.Batch.Service/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using ServiceHub.Batch.Service.Controllers;
 
 namespace ServiceHub.Batch.Service
 {
