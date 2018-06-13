07b8008640a7eb6a1edbb392cf7687de043cd12c
Sebastian Majko <smajko31@gmail.com>
2018-06-13 16:00:15 -0400

    add interface for repo injection [#157252773]

---
+++ src/ServiceHub.Batch.Context/Utilities/IUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceHub.Batch.Context.Utilities
+{
+    /// <summary>
+    /// Basic CRUD interface for Batch models
+    /// All storage implementations must inherit IUtility
+    /// </summary>
+    public interface IUtility
+    {
+        List<Library.Models.Batch> GetBatchesBySkill(string skill);
+        List<Library.Models.Batch> GetBatchesByLocation(string city, string state);
+        List<Library.Models.Batch> GetAllBatches();
+        void AddBatch(Library.Models.Batch batch);
+        void UpdateBatch(Library.Models.Batch batch);
+        void DeleteBatch(Guid id);
+    }
+}
